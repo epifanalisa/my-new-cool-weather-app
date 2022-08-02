48ca137ffb54d60d7de98353d0e48ae9faaf98c5
epifanalisa <epifanalisa@gmail.com>
2022-08-02 17:51:03 +0300

    Changed forecast icon size

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function displayForecast(response) {
                     forecastDay.weather[0].icon
                   }@2x.png"
                   alt=""
-                  width="60px"
+                  width="50px"
                 />
                 <div class="forecast-temperatures">
                   <span class="forecast-temp-max">${Math.round(
